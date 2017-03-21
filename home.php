c9f22a28a0319965f60d8524878bbfb2b4c2ace2
Marcus Geil <mg@writereader.com>
2017-03-21 11:49:55 +0100

    Blog page now works

---
+++ home.php
@@ -0,0 +1,41 @@
+<?php
+get_header(); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs">
+            <div class="content-card">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h1 class="content-card-title"><?php single_post_title(); ?></h1>
+                    </div>
+                    <div class="col-xs-12">
+                        <div class="row">
+                            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                                <div class="col-xs-12 col-sm-6 col-md-4">
+                                    <div class="post-card">
+                                        <a href="<?php the_permalink(); ?>">
+                                            <div class="post-card-image">
+                                                <h2 class="post-card-title"><?php the_title(); ?></h2>
+                                            </div>
+                                            <div class="post-card-body">
+                                                <p><?php the_content(); ?></p>
+                                            </div>
+                                        </a>
+                                        <paper-ripple></paper-ripple>
+                                    </div>
+                                </div>
+                            <?php endwhile; else : ?>
+                                <div class="col-xs">
+                                    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
