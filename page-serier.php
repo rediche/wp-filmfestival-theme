9cb59f2badd74cee46b4550e67d336846db9e78c
Marcus Geil <mg@writereader.com>
2017-03-27 11:35:01 +0200

    Series updates

--- page-serier.php
+++ page-serier.php
@@ -13,6 +13,39 @@
             </div>
             <?php endwhile; endif; ?>
 
+            <?php 
+                $series = get_terms( array(
+                    'taxonomy' => 'series',
+                    'hide_empty' => true
+                ) );
+            ?>
+
+            <?php foreach($series as $collection) : ?>
+                <div class="content-card slider-card">
+                    <h2 class="content-card-extras-title no-margin"><?php echo $collection->name; ?></h2>
+                    <div class="slide-items row">
+                        <?php
+                            $args = array( 
+                                'post_type' => 'movie',
+                                'tax_query' => array( 
+                                    array(
+                                        'taxonomy' => 'series',
+                                        'field' => 'slug',
+                                        'terms' => $collection->slug
+                                    )
+                                )
+                            );
+
+                            $seriesMovieLoop = new WP_Query( $args );
+                            while ( $seriesMovieLoop->have_posts() ) : $seriesMovieLoop->the_post();
+                                get_template_part( 'partials/frontpage/loop', 'movie' );
+                            endwhile;
+                        ?>
+                    </div>
+                </div>
+                
+            <?php endforeach; ?>
+
             <div class="content-card slider-card">
                 <h2 class="content-card-extras-title no-margin">Udvalgte film</h2>
                 <div class="slider-items row">
