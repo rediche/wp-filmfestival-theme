209e6f1930e37f183676c8e6dcb26d8f3db30f34
Marcus Geil <mg@writereader.com>
2017-03-27 11:47:20 +0200

    Added max movies per series list

--- page-serier.php
+++ page-serier.php
@@ -31,6 +31,7 @@
                         <?php
                             $args = array( 
                                 'post_type' => 'movie',
+                                'posts_per_page' => 3,
                                 'tax_query' => array( 
                                     array(
                                         'taxonomy' => 'series',
@@ -49,19 +50,6 @@
                 </div>
                 
             <?php endforeach; ?>
-
-            <div class="content-card slider-card">
-                <h2 class="content-card-extras-title no-margin">Udvalgte film</h2>
-                <div class="slider-items row">
-                    <?php
-                        $args = array( 'post_type' => 'movie', 'posts_per_page' => 3 );
-                        $selectedMoviesLoop = new WP_Query( $args );
-                        while ( $selectedMoviesLoop->have_posts() ) : $selectedMoviesLoop->the_post();
-                            get_template_part( 'partials/frontpage/loop', 'movie' );
-                        endwhile;
-                    ?>
-                </div>
-            </div>
         </div>
     </div>
 </div>
