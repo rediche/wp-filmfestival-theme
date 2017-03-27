d924ec613fda1ecf2d9960bdec9aa77d048df3bf
Marcus Geil <mg@writereader.com>
2017-03-27 08:40:04 +0200

    Added mypix page template

--- page-mypix.php
+++ page-mypix.php
@@ -1,11 +1,14 @@
-<?php include('header.php'); ?>
+<?php 
+/* Template Name: MyPIX */
+?>
+<?php get_header(); ?>
 
 <div class="mypix">
     <div class="container">
         <div class="row">
             <div class="col-xs">
                 <div class="content-card">
-                    <h1 class="content-card-title">MyPix</h1>
+                    <h1 class="content-card-title"><?php the_title(); ?></h1>
                     <h2 class="content-card-extras-title">Mit program</h2>
                     <div class="movie-schedule" data-ticket-list></div>
                 </div>
@@ -22,4 +25,4 @@
     </div>
 </div>
 
-<?php include('footer.php'); ?>
+<?php get_footer(); ?>
