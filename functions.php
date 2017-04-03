517f269790c1236a50022da2efc4850efd82ddff
Marcus Geil <mg@writereader.com>
2017-04-03 11:40:40 +0200

    Added person shortcode

--- functions.php
+++ functions.php
@@ -28,3 +28,34 @@ function register_main_menu() {
   register_nav_menu( 'primary', __( 'Primary Menu', 'cphpix' ) );
 }
 add_action( 'after_setup_theme', 'register_main_menu' );
+
+/* THIS SHOULD BE IN A PLUGIN!!! */
+// [person name="" image="" title="" mail="" tel=""]
+function cphpix_init_person_shortcode( $atts ) {
+    $attributes = shortcode_atts( array(
+        'name' => 'Navn her',
+        'image' => 'Billede URL',
+        'title' => 'Titel her',
+        'mail' => 'Mail her',
+        'tel' => 'Telefonnummer her'
+    ), $atts );
+
+    ob_start(); ?>
+
+    <div class="person-card">
+        <div class="person-card-image">
+            <img src="<?php echo $attributes['image']; ?>" alt="<?php echo $attributes['name']; ?>">
+        </div>
+        <div class="person-card-content">
+            <p class="person-card-name"><?php echo $attributes['name']; ?></p>
+            <p class="person-card-title"><?php echo $attributes['title']; ?></p>
+            <p class="person-card-mail"><a href="mailto:<?php echo $attributes['mail']; ?>" title="Send mail til <?php echo $attributes['name']; ?>"><?php echo $attributes['mail']; ?></a></p>
+            <p class="person-card-tel"><a href="tel:<?php echo $attributes['tel']; ?>" title="Ring til <?php echo $attributes['name']; ?>"><?php echo $attributes['tel']; ?></a></p>
+        </div>
+    </div>
+
+    <?php
+    return ob_get_clean();
+
+}
+add_shortcode( 'person', 'cphpix_init_person_shortcode' );
