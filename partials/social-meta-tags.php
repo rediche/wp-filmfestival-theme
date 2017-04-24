e26c8d9da82a360a8620e256ad9c716fe6e1cf91
Marcus Geil <mg@writereader.com>
2017-04-24 09:20:07 +0200

    Added descriptive comments

--- partials/social-meta-tags.php
+++ partials/social-meta-tags.php
@@ -1,3 +1,6 @@
+<!-- Data som sociale medier bruger når man deler.
+     Google bruger også nogle af disse data til Søgemaskineoptimering -->
+
 <!-- Twitter Card data -->
 <meta name="twitter:card" content="summary" />
 <meta name="twitter:site" content="@kbhfilm" />
@@ -13,11 +16,15 @@
     <meta property="og:image" content="<?php the_post_thumbnail_url(); ?>" />
 <?php endif; ?>
 
+
+<!-- Hvis siden har indhold -->
 <?php if ($post->post_content != '') : ?>
+    <!-- Lav meta description fra indholdet -->
     <meta name="description" content="<?php echo substr(wp_strip_all_tags($post->post_content), 0, 152) . '...'; ?>" />
     <meta property="og:description" content="<?php echo substr(wp_strip_all_tags($post->post_content), 0, 152) . '...'; ?>" />
     <meta property="twitter:description" content="<?php echo substr(wp_strip_all_tags($post->post_content), 0, 197) . '...'; ?>" />
 <?php else : ?>
+    <!-- Ellers brug en standard beskrivelse -->
     <meta name="description" content="KBHFILM 2017 er ikke en rigtig filmfestival. Men siden er fed, ikke?" />
     <meta property="og:description" content="KBHFILM 2017 er ikke en rigtig filmfestival. Men siden er fed, ikke?" />
 <?php endif; ?>
