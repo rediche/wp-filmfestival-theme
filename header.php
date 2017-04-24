0b74a4a18100d131ca3681184cca3edc6aa222b2
Marcus Geil <mg@writereader.com>
2017-04-24 09:21:58 +0200

    Removed unused dependencies

--- header.php
+++ header.php
@@ -12,15 +12,11 @@
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+  <!-- HTML Imports så vi kan lave vores lille toast popup
+       og så vi kan få vores ripple effekt på knapperne -->
   <script src="<?php echo get_template_directory_uri(); ?>/bower_components/webcomponentsjs/webcomponents-lite.js"></script>
   <link rel="import" href="<?php echo get_template_directory_uri(); ?>/bower_components/paper-ripple/paper-ripple.html">
   <link rel="import" href="<?php echo get_template_directory_uri(); ?>/bower_components/paper-toast/paper-toast.html">
-  <link rel="import" href="<?php echo get_template_directory_uri(); ?>/bower_components/iron-form/iron-form.html">
-  <link rel="import" href="<?php echo get_template_directory_uri(); ?>/bower_components/paper-radio-group/paper-radio-group.html">
-  <link rel="import" href="<?php echo get_template_directory_uri(); ?>/bower_components/paper-checkbox/paper-checkbox.html">
-  <link rel="import" href="<?php echo get_template_directory_uri(); ?>/bower_components/paper-listbox/paper-listbox.html">
-  <link rel="import" href="<?php echo get_template_directory_uri(); ?>/bower_components/paper-item/paper-item.html">
-  <link rel="import" href="<?php echo get_template_directory_uri(); ?>/bower_components/paper-dropdown-menu/paper-dropdown-menu.html">
 
   <?php wp_head(); ?>
 </head>
